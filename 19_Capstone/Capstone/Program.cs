2130615c2505ea84c4b098a4412491c61b26450c
Lisa <lisalynn103187@gmail.com>
2020-02-06 11:07:04 -0500

    Stock method

--- 19_Capstone/Capstone/Program.cs
+++ 19_Capstone/Capstone/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Capstone
 {
@@ -6,7 +8,39 @@ namespace Capstone
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            public void StockItems()
+            {
+                //read csv file and split by line
+                string filename = "vendingmachine.csv";
+                List<string> allWords = new List<string>();
+                using (StreamReader sr = new StreamReader(filename))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        string[] words = line.Split("\n");
+                        allWords.AddRange(words);
+
+                    }
+                }
+                //join lines with ',' and then split and create into array
+                string newWordStr = string.Join(",", allWords);
+                string[] splitByLine = newWordStr.Split(",");
+
+                //loop through and assign array values to variables
+                for (int i = 1; i < splitByLine.Length - 1; i++)
+                {
+                    string slot = splitByLine[0];
+                    string itemName = splitByLine[1];
+                    string itemPrice = splitByLine[2];
+                    double itemPriceInt = double.Parse(itemPrice);
+                    string itemType = splitByLine[3];
+                }
+            }
+
+
+
+
         }
     }
 }
